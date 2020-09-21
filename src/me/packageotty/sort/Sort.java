671a34c9bb102ea789a9d737934626af6b69e262
FPercival <44174895+FCPercival@users.noreply.github.com>
2020-09-21 18:42:44 +0200

    added sort

--- src/me/packageotty/sort/Sort.java
+++ src/me/packageotty/sort/Sort.java
@@ -19,16 +19,15 @@ public class Sort {
     public static void sort(@NotNull Integer[] input,@NotNull boolean decreasing){
 
         if(decreasing){
-            for (int i = 0; i < input.length-1; i++) {
+            Arrays.sort(input);
+            for (int i=0; i< input.length;i++){
                 for (int j = 0; j < i+1; j++) {
-                    if (input[i] < input[j]) {
-                        int temp = 0;
-                        temp = input[j];
-                        input[j] = input[i];
-                        input[i] = temp;
-                    }
-
+                    int temp = 0;
+                    temp = input[i];
+                    input[i] = input[j];
+                    input[j] = temp;
                 }
+
             }
         }else {
             Arrays.sort(input);
@@ -39,16 +38,15 @@ public class Sort {
 
     public void sort(@NotNull Float[] input,@NotNull boolean decreasing){
         if(decreasing){
-            for (int i = 0; i < input.length-1; i++) {
+            Arrays.sort(input);
+            for (int i=0; i< input.length;i++){
                 for (int j = 0; j < i+1; j++) {
-                    if (input[i] < input[j]) {
-                        float temp = 0;
-                        temp = input[j];
-                        input[j] = input[i];
-                        input[i] = temp;
-                    }
-
+                    float temp = 0;
+                    temp = input[i];
+                    input[i] = input[j];
+                    input[j] = temp;
                 }
+
             }
         }else {
             Arrays.sort(input);
@@ -57,16 +55,15 @@ public class Sort {
     }
     public void sort(@NotNull Double[] input,@NotNull boolean decreasing){
         if(decreasing){
-            for (int i = 0; i < input.length-1; i++) {
+            Arrays.sort(input);
+            for (int i=0; i< input.length;i++){
                 for (int j = 0; j < i+1; j++) {
-                    if (input[i] < input[j]) {
-                        double temp = 0;
-                        temp = input[j];
-                        input[j] = input[i];
-                        input[i] = temp;
-                    }
-
+                    double temp = 0;
+                    temp = input[i];
+                    input[i] = input[j];
+                    input[j] = temp;
                 }
+
             }
         }else {
             Arrays.sort(input);
@@ -75,16 +72,15 @@ public class Sort {
     }
     public void sort(@NotNull Short[] input,@NotNull boolean decreasing){
         if(decreasing){
-            for (int i = 0; i < input.length-1; i++) {
+            Arrays.sort(input);
+            for (int i=0; i< input.length;i++){
                 for (int j = 0; j < i+1; j++) {
-                    if (input[i] < input[j]) {
-                        short temp = 0;
-                        temp = input[j];
-                        input[j] = input[i];
-                        input[i] = temp;
-                    }
-
+                    short temp = 0;
+                    temp = input[i];
+                    input[i] = input[j];
+                    input[j] = temp;
                 }
+
             }
         }else {
             Arrays.sort(input);
@@ -93,16 +89,15 @@ public class Sort {
     }
     public void sort(@NotNull Long[] input,@NotNull boolean decreasing){
         if(decreasing){
-            for (int i = 0; i < input.length-1; i++) {
+            Arrays.sort(input);
+            for (int i=0; i< input.length;i++){
                 for (int j = 0; j < i+1; j++) {
-                    if (input[i] < input[j]) {
-                        Long temp = 0L;
-                        temp = input[j];
-                        input[j] = input[i];
-                        input[i] = temp;
-                    }
-
+                    long temp = 0;
+                    temp = input[i];
+                    input[i] = input[j];
+                    input[j] = temp;
                 }
+
             }
         }else {
             Arrays.sort(input);
@@ -111,12 +106,12 @@ public class Sort {
     }
 
 
-    public static void main(String[] args) {
-        Integer[] input = {12, 2211, 1, 3, 11111
-                , 2222, 3235, 65435, 12};
-        sort(input, false);
-        for(int s : input) {
-            System.out.println(s);
-        }
-    }
+    //public static void main(String[] args) {
+    //    Integer[] input = {12, 2211, 1, 3, 11111
+    //            , 2222, 3235, 65435, 12};
+    //    sort(input, true);
+    //    for(int s : input) {
+    //        System.out.println(s);
+    //    }
+    //}
 }
